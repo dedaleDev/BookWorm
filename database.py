e300bb33fcc1ef7ee716f25d7d70bb7756a0672f
Diéval Flavien <dievalflavien@protonmail.com>
2024-03-30 19:06:10 +0100

    improved connexion

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 import pymysql
 
 class db():
-    def __init__(self) -> None:
-        try :
-            db =pymysql.connect(host="192.168.1.20", charset="utf8",user="root", passwd="1234",db="BookWorm")
-        except Exception as e:
-            print(f"Erreur : Impossible de se connecter à la base de donnée : {e}")
+    def __init__(self, host:str ="localhost", user:str="root", passwd:str="1234", db:str="BookWorm") -> None:
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        #-----initialisation de la base de donnée-----
+        self.db = pymysql.connect(host=self.host, user=self.user, passwd=self.passwd, db=self.db)
