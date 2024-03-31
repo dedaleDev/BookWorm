6cd1ceab634458fc1aa41f27fbb815adbbd7173d
Diéval Flavien <dievalflavien@protonmail.com>
2024-03-31 10:06:01 +0200

    added create_db

--- database.py
+++ database.py
@@ -1,9 +1,38 @@
 import pymysql
 
 class db():
-    def __init__(self, host:str ="localhost", user:str="root", passwd:str="1234", db:str="BookWorm") -> None:
+    def __init__(self, host:str ="localhost", user:str="root", passwd:str="1234") -> None:
         self.host = host
         self.user = user
         self.passwd = passwd
         #-----initialisation de la base de donnée-----
-        self.db = pymysql.connect(host=self.host, user=self.user, passwd=self.passwd, db=db)
+        try :
+            self.db = pymysql.connect(host=self.host, charset="utf8mb4",user=self.user, passwd=self.passwd, db="BookWorm")
+            self.cursor = self.db.cursor()
+        except : 
+            print("Base inexistante, tentative de création de la base de donnée...")
+            try :
+                self.create_db()
+            except :
+                print("Création de la base de donnée échouée ! Veillez verifier vos paramètres de connexion.")
+                exit()
+
+    def create_db(self) -> None:
+        self.db = pymysql.connect(host=self.host, charset="utf8mb4", user=self.user, passwd=self.passwd)
+        self.cursor = self.db.cursor()
+        try:
+            with open("BookWorm.sql") as f:
+                sql = f.read()
+                self.cursor.execute(sql)
+                self.cursor.fetchall()
+                self.db.commit()
+        except:
+            print("Impossible de créer la base de donnée ! Le fichier BookWorm.sql est introuvable.")
+            exit()
+
+
+        
+        self.db = pymysql.connect(host=self.host, charset="utf8mb4", user=self.user, passwd=self.passwd, db=db)
+
+
+    
